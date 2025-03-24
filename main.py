f94c60064a77ead7efb69c94a7e77acaa9208b78
B1gB4dB4ng <abdullahsamurnl@gmail.com>
2025-03-24 10:10:38 +0100

    creating new user

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-
+from routers import user
 from db import models
 from db.database import engine
 
 app = FastAPI()
+app.include_router(user.router)
 
 
 @app.get("/")

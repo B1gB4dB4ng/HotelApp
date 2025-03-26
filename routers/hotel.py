0eee073361a37365d23ba19cf86117ff172aaf3b
B1gB4dB4ng <abdullahsamurnl@gmail.com>
2025-03-26 09:32:36 +0100

    combined filter and search, changed the order of the routers

--- routers/hotel.py
+++ routers/hotel.py
@@ -11,17 +11,6 @@ from typing import Optional, List
 router = APIRouter(prefix="/hotel", tags=["Hotel"])
 
 
-# Delete Hotel
-@router.get(
-    "/{id}/delete",
-    tags=["Hotel"],
-    summary="Remove hotel",
-    description="This API call removes hotel from db",
-)
-def delete(id: int, db: Session = Depends(get_db)):
-    return db_hotel.delete_hotel(db, id)
-
-
 @router.post("/submit", response_model=HotelDisplay)
 def submit_hotel(request: HotelBase, db: Session = Depends(get_db)):
     # For now we'll fake the user ID (you'll replace with real one later)
@@ -29,22 +18,10 @@ def submit_hotel(request: HotelBase, db: Session = Depends(get_db)):
     return db_hotel.create_hotel(db, request)
 
 
-# read all hotels
-@router.get("/all", response_model=list[HotelDisplay])
-def get_all_hotels(db: Session = Depends(get_db)):
-    return db_hotel.get_all_hotels(db)
-
-
 # raed one hotel
 @router.get("/{id}", response_model=HotelDisplay)
 def get_hotel(id: int, db: Session = Depends(get_db)):
     return db_hotel.get_hotel(db, id)
-
-
-# update hotels
-@router.post("/{id}/update")
-def update_hotel(id: int, request: HotelBase, db: Session = Depends(get_db)):
-    return db_hotel.update_hotel(db, id, request)
 
 
 # Combine search and filter logic into one endpoint
@@ -81,3 +58,20 @@ def get_hotels(
         return filtered_hotels
 
     return hotels
+
+
+# update hotels
+@router.post("/{id}/update")
+def update_hotel(id: int, request: HotelBase, db: Session = Depends(get_db)):
+    return db_hotel.update_hotel(db, id, request)
+
+
+# Delete Hotel
+@router.get(
+    "/{id}/delete",
+    tags=["Hotel"],
+    summary="Remove hotel",
+    description="This API call removes hotel from db",
+)
+def delete(id: int, db: Session = Depends(get_db)):
+    return db_hotel.delete_hotel(db, id)

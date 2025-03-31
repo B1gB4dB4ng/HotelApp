f141b7c4c709ec1f2e6d201dd7715793d0b903a7
samteym <your_email@example.com>
2025-03-31 14:53:19 +0200

    test

---
+++ routers/review.py
@@ -0,0 +1,6 @@
+# from fastapi import APIRouter, Depends, HTTPException
+# from sqlalchemy.orm import Session
+# from database import get_db
+# import db_review  
+
+# router = APIRouter(prefix="/reviews", tags=["Reviews"])
